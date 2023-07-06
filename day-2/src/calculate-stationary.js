0f15525ad89bd5542efdabf2d99392affcf03a59
Debdutta <m-debdutta@users.noreply.github.com>
2023-07-06 18:31:37 +0530

    AOC day 2, part-1 done

--- day-2/src/calculate-stationary.js
+++ day-2/src/calculate-stationary.js
@@ -1,5 +1,10 @@
-const calculateWrapingPaper = () => {
-  return 7;
+const calculateWrapingPaper = (giftSize) => {
+  const dimension = giftSize.split('x').map((a) => +a);
+  dimension.sort((a, b) => a - b);
+  const [l, w, h] = dimension;
+  const paperRequired = (2 * l * w) + (2 * w * h) + (2 * h * l) + (l * w);
+  const requiredWrapingPaper = paperRequired;
+  return requiredWrapingPaper;
 }
 
-module.exports = {calculateWrapingPaper};
+module.exports = { calculateWrapingPaper };
--- day-2/test/calculate-stationary-test.js
+++ day-2/test/calculate-stationary-test.js
@@ -1,13 +1,21 @@
 const { describe, it } = require('node:test');
 const { strictEqual } = require('assert');
-const {calculateWrapingPaper} = require('../src/calculate-stationary.js');
+const { calculatePaperToBeOrdered } = require('../src/calculate-stationary.js');
 
 describe('calculateWrapingPaper', () => {
   it('should calculate the wraping paper needed to wrap a gift of unit dimension', () => {
-    strictEqual(calculateWrapingPaper('1x1x1'), 7);
+    strictEqual(calculatePaperToBeOrdered('1x1x1'), 7);
   });
 
-  it('should calculate the wraping paper needed to wrap a gift of 2x3x4 dimension', () => {
-    strictEqual(calculateWrapingPaper('2x3x4'), 58);
+  it('should calculate the wraping paper needed for a gift of 2x3x4 dimension', () => {
+    strictEqual(calculatePaperToBeOrdered('2x3x4'), 58);
+  });
+
+  it('should calculate the wraping paper needed for a gift of 1x1x10 dimension', () => {
+    strictEqual(calculatePaperToBeOrdered('1x1x10'), 43);
+  });
+
+  it('should calculate the wraping paper needed for a gift of 1x1x10 dimension, when the dimention are unorderd', () => {
+    strictEqual(calculatePaperToBeOrdered('4x2x3'), 58);
   });
 });
