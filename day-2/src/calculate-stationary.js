64f8c8a6ebd457f229b14ed0627c9f8db715aa2e
Debdutta <m-debdutta@users.noreply.github.com>
2023-07-06 18:35:03 +0530

    pulled out parseDimension and toNumber into a function

--- day-2/src/calculate-stationary.js
+++ day-2/src/calculate-stationary.js
@@ -1,6 +1,13 @@
-const calculatePaperToBeOrdered = (giftSize) => {
-  const dimension = giftSize.split('x').map((a) => +a);
+const toNumber = a => +a;
+
+const parseDimension = (giftSize) => {
+  const dimension = giftSize.split('x').map(toNumber);
   dimension.sort((a, b) => a - b);
+  return dimension;
+}
+
+const calculatePaperToBeOrdered = (giftSize) => {
+  const dimension = parseDimension(giftSize);
   const [l, w, h] = dimension;
   const slackPaper = l * w;
   const paperToWrap = (2 * l * w) + (2 * w * h) + (2 * h * l);
@@ -9,3 +16,5 @@ const calculatePaperToBeOrdered = (giftSize) => {
 }
 
 module.exports = { calculatePaperToBeOrdered };
+
+
