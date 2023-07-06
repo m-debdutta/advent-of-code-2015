f9c55afcf9023fa3fbe39452f58a56c1d0e7f34f
Debdutta <m-debdutta@users.noreply.github.com>
2023-07-06 18:33:42 +0530

    changed function name and pulled out calculations into variables

--- day-2/src/calculate-stationary.js
+++ day-2/src/calculate-stationary.js
@@ -1,10 +1,11 @@
-const calculateWrapingPaper = (giftSize) => {
+const calculatePaperToBeOrdered = (giftSize) => {
   const dimension = giftSize.split('x').map((a) => +a);
   dimension.sort((a, b) => a - b);
   const [l, w, h] = dimension;
-  const paperRequired = (2 * l * w) + (2 * w * h) + (2 * h * l) + (l * w);
-  const requiredWrapingPaper = paperRequired;
-  return requiredWrapingPaper;
+  const slackPaper = l * w;
+  const paperToWrap = (2 * l * w) + (2 * w * h) + (2 * h * l);
+  const paperRequired = paperToWrap + slackPaper;
+  return paperRequired;
 }
 
-module.exports = { calculateWrapingPaper };
+module.exports = { calculatePaperToBeOrdered };
